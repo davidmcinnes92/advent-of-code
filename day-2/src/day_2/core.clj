30b253bdb5a72dcb0ea312f52fba56bfbdc0798e
David McInnes <davidmcinnes92@gmail.com>
2021-12-05 01:13:38 +0000

    Final solution day 2 part 1

--- day-2/src/day_2/core.clj
+++ day-2/src/day_2/core.clj
@@ -1,9 +1,31 @@
 (ns day-2.core
   (:gen-class))
 
+(require '[clojure.string :as str])
+
 (def input (slurp "resources/input.txt"))
+(def input-test (slurp "resources/input-test.txt"))
+(def lines (str/split-lines input))
+(def horizontal-position (atom 0))
+(def depth (atom 0))
+
+(defn dive [command units]
+  (cond
+    (= command "forward") (reset! horizontal-position (+ @horizontal-position units))
+    (= command "down") (reset! depth (+ @depth units))
+    (= command "up") (reset! depth (- @depth units))))
+
+(defn process-input [input]
+  (doseq [x input]
+    (def instruction (str/split x #" "))
+    (def command (nth instruction 0))
+    (def units (Long/parseLong (nth instruction 1)))
+    (dive command units)))
+
+(defn position-depth-product []
+  (* @horizontal-position @depth))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+  (process-input lines)
+  (println "The position depth product is:" (position-depth-product)))
