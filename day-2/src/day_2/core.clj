312118754b0a5d6319d95c295a1d52a779b6d229
David McInnes <davidmcinnes92@gmail.com>
2021-12-08 18:57:24 +0000

    Day 2 fully working solution.

--- day-2/src/day_2/core.clj
+++ day-2/src/day_2/core.clj
@@ -8,6 +8,13 @@
 (def lines (str/split-lines input))
 (def horizontal-position (atom 0))
 (def depth (atom 0))
+(def aim (atom 0))
+
+(defn position-depth-product []
+  (* @horizontal-position @depth))
+
+(defn depth-increase [horizontal-position-increase aim]
+  (* horizontal-position-increase aim))
 
 (defn dive [command units]
   (cond
@@ -15,15 +22,21 @@
     (= command "down") (reset! depth (+ @depth units))
     (= command "up") (reset! depth (- @depth units))))
 
+(defn dive-aim [command units]
+  (cond
+    (= command "forward") (do 
+      (reset! horizontal-position (+ @horizontal-position units))
+      (def depth-increase-total (depth-increase units @aim))
+      (if (= depth-increase-total 0) (println "Aim is 0 - so no depth increase") (reset! depth (+ @depth depth-increase-total))))
+    (= command "down") (reset! aim (+ @aim units))
+    (= command "up") (reset! aim (- @aim units))))
+
 (defn process-input [input]
   (doseq [x input]
     (def instruction (str/split x #" "))
     (def command (nth instruction 0))
     (def units (Long/parseLong (nth instruction 1)))
-    (dive command units)))
-
-(defn position-depth-product []
-  (* @horizontal-position @depth))
+    (dive-aim command units)))
 
 (defn -main
   [& args]
