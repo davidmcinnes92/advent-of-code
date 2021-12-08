2c4c7173d9c5426292533f4ea57f3100730fd140
David McInnes <davidmcinnes92@gmail.com>
2021-12-08 22:41:37 +0000

    day 3 part 1 complete

--- day-3/src/day_3/core.clj
+++ day-3/src/day_3/core.clj
@@ -12,23 +12,26 @@
                   }})
 
 (def input (slurp "resources/input.txt"))
-;;(def input-test (slurp "resources/input_test.txt"))
+(def input-test (slurp "resources/test-input.txt"))
 (def numbers (str/split-lines input))
 ;;(def 2d-numbers (to-array-2d input))
 
-;; (defn gamma-rate []
-;;   (def indexer (atom 0))
-;;   (def total-0 (atom 0))
-;;   (def total-1 (atom 0))
-;;   (def results-array (make-array Integer/TYPE 8))
-;;   (aset-int results-array @indexer 0)
-;;   (while (< @indexer 8)
-;;     (do (doseq [x numbers]
-;;       (def binary-element (get x @indexer))
-;;       (if (= binary-element 0) (swap! total-0 inc) (swap! total-1 inc))))))
-;;     (if (> @total-0 @total-1) (aset-int results-array @indexer 0) (aset-int results-array @indexer 1))))
-;;     (swap! indexer inc))
-;;   (println results-array))
+(defn gamma-rate []
+  (def indexer (atom 0))
+  (def total-0 (atom 0))
+  (def total-1 (atom 0))
+  (def results-array (make-array Integer/TYPE 12))
+  (while (< @indexer 12)
+    (do (doseq [x numbers]
+      (def binary-element (Character/digit (get x @indexer) 12))
+      (if (= binary-element 0) (swap! total-0 inc))
+      (if (= binary-element 1) (swap! total-1 inc))))
+  (if (< @total-0 @total-1) (aset-int results-array @indexer 0) (aset-int results-array @indexer 1))
+  (reset! total-0 0)
+  (reset! total-1 0)
+  (swap! indexer inc)
+  (println "indexer is" @indexer))
+  (println (java.util.Arrays/toString results-array)))
 
 (defn epsilon-rate []
   (def indexer (atom 0))
@@ -39,11 +42,12 @@
     (do (doseq [x numbers]
       (def binary-element (Character/digit (get x @indexer) 12))
       (if (= binary-element 0) (swap! total-0 inc))
-      (if (= binary-element 1) (swap! total-1 inc))
-  (if (> @total-0 @total-1) (aset-int results-array @indexer 0) (aset-int results-array @indexer 1))))
-  (println "total-0 is" @total-0)
-  (println "total-1 is" @total-1)
-  (swap! indexer inc))
+      (if (= binary-element 1) (swap! total-1 inc))))
+  (if (> @total-0 @total-1) (aset-int results-array @indexer 0) (aset-int results-array @indexer 1))
+  (reset! total-0 0)
+  (reset! total-1 0)
+  (swap! indexer inc)
+  (println "indexer is" @indexer))
   (println (java.util.Arrays/toString results-array)))
 
 (defn if-test []
@@ -53,7 +57,8 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  ;;(if-test)
+  (gamma-rate)
+  (epsilon-rate)
   (println directions)
   (def sub-directions (:south-west directions))
   (map remove sub-directions)
@@ -61,4 +66,7 @@
   (println (:cool sub-directions))
   (println (:north directions))
   (println (directions :south))
+  (println "number is "(Integer/parseInt "110011110100" 2))
+  (println "number is "(Integer/parseInt "001100001011" 2))
+  (println (* 3316 779))
   (println "Hello, World!"))
